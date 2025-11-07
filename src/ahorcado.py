a5c2c9798f0e927a8b00c70469e7646697dffe5a
dtorresy <dtorresy@iesrafaelalberti.es>
2025-11-07 09:00:43 +0100

    Realizacion resto funciones ahorcado

--- src/ahorcado.py
+++ src/ahorcado.py
@@ -38,25 +38,30 @@ def solicitar_palabra():
     palabra = ""
 
     while len(palabra) < 5:
-        # Hacer la solicitud GET dentro del ciclo para obtener una nueva palabra cada vez
         response = requests.get(url)
         if response.status_code == 200:
             data = response.json()
             palabra = data[0]["word"]
             if len(palabra) < 5:
-                palabra = ""  # Vaciar para forzar otra iteración si palabra tiene menos de 5 letras
+                palabra = ""
         else:
             print("Error al obtener la palabra aleatoria")
 
     return palabra.upper()
 
-def aniadir_letras_usadas(letra:str) -> list:
+def ocultar_palabra(palabra:str) -> str:
     
-    letras_usadas = []
+    palabra_oculta = " ".join(["_"] * len(palabra))
+    
+    return palabra_oculta
+    
+def aniadir_letras_usadas(letras_usadas:list, letra:str) -> list:
+    
     letras_usadas.append(letra)
+    
     return letras_usadas
 
-def solicitar_letra(letras_usadas):
+def solicitar_letra(letras_usadas) -> str:
     """
     Solicita una letra al jugador 2
     La letra debe ser válida (solo una letra) y no estar ya usada
@@ -75,10 +80,12 @@ def solicitar_letra(letras_usadas):
     letra = None
     
     while not letra:
-        letra = input("Introduce una letra: ")
+        letra = input("Introduce una letra: ").upper()
+        
         if len(letra) == 1 and letra.isalpha() and not letra in letras_usadas:
             return letra.upper()
         else:
+            print("Debe introducir una letra valida y que no este en letras usadas.")
             letra = None
 
 def mostrar_estado(palabra_oculta, intentos, letras_usadas):
@@ -90,13 +97,11 @@ def mostrar_estado(palabra_oculta, intentos, letras_usadas):
         intentos (int): Número de intentos restantes
         letras_usadas (list): Lista de letras ya usadas
     """
-    # TODO: Implementar la función
     # - Imprimir intentos restantes
     # - Imprimir la palabra con espacios entre caracteres
     # - Imprimir las letras usadas
-    print(intentos)
-
-
+    print(f'La palabra oculta es: {palabra_oculta}\nNumero de intentos: {intentos}\nLetras usadas: {letras_usadas}')
+    
 def actualizar_palabra_oculta(palabra, palabra_oculta, letra):
     """
     Actualiza la palabra oculta revelando las apariciones de la letra
@@ -114,9 +119,15 @@ def actualizar_palabra_oculta(palabra, palabra_oculta, letra):
     # - Usar enumerate() para obtener índice y carácter
     # - Si el carácter coincide con la letra, reemplazar en palabra_oculta
     # - Puedes convertir palabra_oculta a lista, modificar y volver a string
-    for letras in enumerate(palabra)
-
-
+    palabra_oculta_lista = palabra_oculta.split(" ")
+    
+    for indice, caracter in enumerate(palabra):
+        if caracter == letra:
+            palabra_oculta_lista[indice] = letra
+    palabra_oculta = " ".join(palabra_oculta_lista)
+    
+    return palabra_oculta
+    
 def jugar():
     """
     Función principal que ejecuta el juego del ahorcado
@@ -126,21 +137,40 @@ def jugar():
     # Configuración inicial
     INTENTOS_MAXIMOS = 5
     
-    # TODO: Solicitar la palabra al jugador 1
     palabra = solicitar_palabra()
     
-    # TODO: Limpiar la pantalla para que el jugador 2 no vea la palabra
     limpiar_pantalla()
     
     # TODO: Inicializar variables del juego
-    # - palabra_oculta: string con guiones bajos (ej: "_ _ _ _ _")
-    # - intentos: número de intentos restantes
-    # - letras_usadas: lista vacía
-    # - juego_terminado: False
+    
+    palabra_oculta = ocultar_palabra(palabra) # - palabra_oculta: string con guiones bajos (ej: "_ _ _ _ _")
+    intentos = INTENTOS_MAXIMOS# - intentos: número de intentos restantes
+    letras_usadas = [] # - letras_usadas: lista vacía
+    juego_terminado = False
     
     print("Jugador 2: ¡Adivina la palabra!\n")
     
     # TODO: Bucle principal del juego
+    
+    while intentos > 0 and not juego_terminado:
+        mostrar_estado(palabra_oculta, intentos, letras_usadas)
+        letra = solicitar_letra(letras_usadas)
+        letras_usadas = aniadir_letras_usadas(letras_usadas, letra)
+        
+        if letra in palabra:
+            palabra_oculta = actualizar_palabra_oculta(palabra, palabra_oculta, letra)
+            print(f"La letra {letra} esta en la palabra")
+            
+            if not "_" in palabra_oculta:
+                juego_terminado = True
+        else:
+            intentos -= 1
+            print(f"Error, la letra {letra} no esta en la palabra.")
+            
+            
+    if not juego_terminado:
+        print(f"¡Has perdido! Se te acabaron los intentos.")
+        print(f"La palabra era: {palabra}")
     # - Mientras haya intentos y el juego no haya terminado:
     #   1. Mostrar el estado actual
     #   2. Solicitar una letra
@@ -153,10 +183,14 @@ def jugar():
     #      - Restar un intento
     #      - Mostrar mensaje de fallo
     
+    
     # TODO: Mostrar mensaje final
+    if juego_terminado:
+        print(f"Enhorabuena ganaste\nLa palabra era {palabra}")
+    else:
+        print(f"Perdiste\nLa palabra era {palabra}")
     # - Si ganó: mostrar felicitación y la palabra
     # - Si perdió: mostrar mensaje de derrota y la palabra correcta
-    pass
 
 
 def main():
@@ -165,11 +199,15 @@ def main():
     """
     jugar()
     
+    jugar_otra_vez = None
     # TODO (Opcional): Preguntar si quiere jugar otra vez
-    # jugar_otra_vez = input("\n¿Quieres jugar otra vez? (s/n): ")
-    # if jugar_otra_vez.lower() == 's':
-    #     main()
-
-
+    while not jugar_otra_vez:
+        
+        jugar_otra_vez = input("\n¿Quieres jugar otra vez? (s/n): ").lower()
+        if jugar_otra_vez != 's' or jugar_otra_vez != 'n':
+            print("Debe introducir (s/n) para saber si seguira jugando")
+            jugar_otra_vez = None
+        elif jugar_otra_vez.lower() == 's':
+            main()
 if __name__ == "__main__":
     main()
